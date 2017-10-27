063986dac583f49293828a9a50d3987c397c3309
Chris Christou <chris.a.christou@gmail.com>
2017-10-27 09:48:16 +0000

    Store classifieds' links. Works for 1st page only

--- test.py
+++ test.py
@@ -2,10 +2,18 @@ from bs4 import BeautifulSoup
 import urllib
 from urllib.request import Request, urlopen
 
-# Request URL as an agent and save the HTML into wepage
+# request URL as an agent and save the HTML into wepage
 req = Request('https://www.car.gr/classifieds/cars/', headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
-
 soup = BeautifulSoup(webpage, "html.parser")
 
-print(soup.prettify())
+# save the HTML inside every classified's div in one page
+classifieds = soup.find_all("div", class_="clsfd_list_row_group")
+
+# store all links into list
+prefix = "http://www.car.gr"
+links = []
+for classified in classifieds:
+    links.append(prefix + classifieds[0].a["href"])
+
+print(links)
