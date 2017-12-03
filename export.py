74ecbe4456c40c90f36a2c37635c909860e51539
Chris Christou <chris.a.christou@gmail.com>
2017-12-03 17:29:01 +0000

    Write headers by retrieving them from db. Add comments

--- export.py
+++ export.py
@@ -1,35 +1,36 @@
 from cs50 import SQL
-# import sqlite3
 import xlsxwriter
 
 def export(table):
     db = SQL("sqlite:///database.db")
-    # conn = sqlite3.connect("database.db")
-    # db = conn.cursor
 
-    workbook = xlsxwriter.Workbook("cars.xlsx")
+    # create workbook
+    workbook = xlsxwriter.Workbook("cars2.xlsx")
     worksheet = workbook.add_worksheet()
 
-    querry = db.execute("SELECT * FROM :table", table=table)
+    # execute database query
+    selection = ("SELECT * FROM {}".format(table))
+    query = db.execute(selection)
 
     row = 1
     col = 0
 
+    # write file headers
     bold = workbook.add_format({'bold': True})
-    worksheet.write('A1', 'Car_Id', bold)
-    worksheet.write('B1', 'Make', bold)
-    worksheet.write('C1', 'Model', bold)
-    worksheet.write('D1', 'Year', bold)
-    worksheet.write('E1', 'Price', bold)
-    worksheet.write('F1', 'Phone', bold)
-    worksheet.write('G1', 'Link', bold)
 
-    for i, entry in enumerate(querry):
+    for key in query[0].keys():
+        worksheet.write(0, col, key, bold)
+        col += 1
+    worksheet.write(0, col, 'link', bold)
+    col = 0
+
+    # write all info from database
+    for i, entry in enumerate(query):
         for key in entry.keys():
             value = entry[key]
             worksheet.write(row, col, value)
             col += 1
-        value = "http://car.gr/{}".format(entry['car_id'])
+        value = "http://car.gr/{}".format(entry['car_id']) # create a link to the classified
         worksheet.write(row, col, value)
         row += 1
         col = 0
