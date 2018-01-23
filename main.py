251a85daa25eb7416543e3ddf3d046f640bb1d25
Chris Christou <chris.a.christou@gmail.com>
2018-01-23 21:33:40 +0000

    Change module name. Simplify date picking

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import datetime
+from datetime import datetime
 from scraper import scrape
 from export import export
-from email import send_email
+from emails import send_email
 
-day = datetime.datetime.now().strftime('%Y-%m-%d')
-day = "{}".format(day)
+today = datetime.today().strftime('%Y-%m-%d').text
 
 url = "https://www.car.gr/classifieds/cars/?offer_type=sale&rg=2&significant_damage=t&st=private"
 # url = "https://www.car.gr/classifieds/cars/?make=18" # for testing purposes
 
 scrape(url)
-export('cars', day)
-send_email(day, 'email@email.com')
+export('cars', today)
+send_email(today, 'email@email.com')
