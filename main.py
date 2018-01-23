57ac6159cd3082007a59416618b12a5ebc9046bb
Chris Christou <chris.a.christou@gmail.com>
2018-01-23 21:40:10 +0000

    Minor fix

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from scraper import scrape
 from export import export
 from emails import send_email
 
-today = datetime.today().strftime('%Y-%m-%d').text
+today = datetime.today().strftime('%Y-%m-%d')
 
-url = "https://www.car.gr/classifieds/cars/?offer_type=sale&rg=2&significant_damage=t&st=private"
-# url = "https://www.car.gr/classifieds/cars/?make=18" # for testing purposes
+# url = "https://www.car.gr/classifieds/cars/?offer_type=sale&rg=2&significant_damage=t&st=private"
+url = "https://www.car.gr/classifieds/cars/?make=18" # for testing purposes
 
 scrape(url)
 export('cars', today)
